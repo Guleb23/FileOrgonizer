ea23df1b4bad0a5d9250d55931e9ceb471a41a20
Guleb23 <112973753+Guleb23@users.noreply.github.com>
2025-06-19 15:09:16 +0300

    Добавьте файлы проекта.

---
+++ Files/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Files
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\юзер\Downloads";
+            
+            string[] files = Directory.GetFiles(path);
+            List<string> allExtentions = new List<string>();
+            foreach (string file in files)
+            {
+                string extention = Path.GetExtension(file);
+
+                allExtentions.Add(extention);
+
+            }
+            CreateFolders(allExtentions, path);
+
+            foreach (string file in files) 
+            {
+                MoveToDirectories(file, path);
+            }
+            Console.WriteLine("All files moved");
+            Console.ReadKey();
+        }
+
+
+        public static void CreateFolders(List<string> exsts, string path)
+        {
+            List<string> uniqExsts = exsts.Distinct().ToList();
+            foreach (string exs in uniqExsts) {
+                string newPath = Path.Combine(path, exs);
+                Directory.CreateDirectory(newPath);
+                Console.WriteLine($"Directory created at path: {newPath}");
+            }
+            Console.WriteLine("Created folders");
+            
+        }
+
+        public static void MoveToDirectories(string file, string path)
+        {
+            string extention = Path.GetExtension(file);
+            string fileName = Path.GetFileName(file);
+            string[] param = {path, extention, fileName};
+            string newPath = Path.Combine(param);
+            File.Move(file, newPath);
+        }
+    }
+}
